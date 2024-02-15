d4790b1a51f39badc157e382cf3182d9969d1438
Jana-Ahmad <jana.abdullah681@gmail.com>
2024-02-15 03:11:00 -0800

    first commit

---
+++ main.js/script.js
@@ -0,0 +1,90 @@
+let counter1 = 0;
+const counterClick1 = document.querySelector(".counter1 .counter-click");
+const resetClick1 = document.querySelector(".counter1 .reset");
+
+counterClick1.onclick = () => {
+  counter1 += 1;
+  document.querySelector(".counter1 .counter-display ").innerHTML = counter1;
+};
+resetClick1.onclick = () => {
+  counter1 = 0;
+  document.querySelector(".counter1 .counter-display ").innerHTML = counter1;
+};
+
+let counter2 = 0;
+const counterClick2 = document.querySelector(".counter2 .counter-click");
+const resetClick2 = document.querySelector(".counter2 .reset");
+
+counterClick2.onclick = () => {
+  counter2 += 1;
+  document.querySelector(".counter2 .counter-display ").innerHTML = counter2;
+};
+resetClick2.onclick = () => {
+  counter2 = 0;
+  document.querySelector(".counter2 .counter-display ").innerHTML = counter2;
+};
+
+let counter3 = 0;
+const counterClick3 = document.querySelector(".counter3 .counter-click");
+const resetClick3 = document.querySelector(".counter3 .reset");
+
+counterClick3.onclick = () => {
+  counter3 += 1;
+  document.querySelector(".counter3 .counter-display ").innerHTML = counter3;
+};
+resetClick3.onclick = () => {
+  counter3 = 0;
+  document.querySelector(".counter3 .counter-display ").innerHTML = counter3;
+};
+
+let counter4 = 0;
+const counterClick4 = document.querySelector(".counter4 .counter-click");
+const resetClick4 = document.querySelector(".counter4 .reset");
+
+counterClick4.onclick = () => {
+  counter4 += 1;
+  document.querySelector(".counter4 .counter-display ").innerHTML = counter4;
+};
+resetClick4.onclick = () => {
+  counter4 = 0;
+  document.querySelector(".counter4 .counter-display ").innerHTML = counter4;
+};
+
+let counter5 = 0;
+const counterClick5 = document.querySelector(".counter5 .counter-click");
+const resetClick5 = document.querySelector(".counter5 .reset");
+
+counterClick5.onclick = () => {
+  counter5 += 1;
+  document.querySelector(".counter5 .counter-display ").innerHTML = counter5;
+};
+resetClick5.onclick = () => {
+  counter5 = 0;
+  document.querySelector(".counter5 .counter-display ").innerHTML = counter5;
+};
+
+let counter6 = 0;
+const counterClick6 = document.querySelector(".counter6 .counter-click");
+const resetClick6 = document.querySelector(".counter6 .reset");
+
+counterClick6.onclick = () => {
+  counter6 += 1;
+  document.querySelector(".counter6 .counter-display ").innerHTML = counter6;
+};
+resetClick6.onclick = () => {
+  counter6 = 0;
+  document.querySelector(".counter6 .counter-display ").innerHTML = counter6;
+};
+
+let counter7 = 0;
+const counterClick7 = document.querySelector(".counter7 .counter-click");
+const resetClick7 = document.querySelector(".counter7 .reset");
+
+counterClick7.onclick = () => {
+  counter7 += 1;
+  document.querySelector(".counter7 .counter-display ").innerHTML = counter7;
+};
+resetClick7.onclick = () => {
+  counter7 = 0;
+  document.querySelector(".counter7 .counter-display ").innerHTML = counter7;
+};
